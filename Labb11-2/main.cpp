1aab44c1f1551b8590ca00296048a013a34fc3e6
Oke Amuwha <gv4281@wayne.edu>
2021-03-08 13:46:57 -0500

    Initial Commit

---
+++ Labb11-2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Labb11-2
+//
+//  Created by Oke-Oghene Amuwha on 3/8/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
